5a76e3d285de0a865c793c18d45e24cf92bc5cfe
Vincent Barrios <32968323+vincentsbarrios@users.noreply.github.com>
2021-03-04 17:53:32 -0800

    done

---
+++ WeatherForecastApp-master/WorkerService/Worker.cs
@@ -0,0 +1,87 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkerService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly EventingBasicConsumer _consumer;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare("reventuki", false, false, false, null);
+            _consumer = new EventingBasicConsumer(_channel);
+        }
+
+        public override async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _consumer.Received += async (model, content) =>
+            {
+                var body = content.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                string something = await do_something(message);
+                DeliveredToConsole(something);
+            };
+            _channel.BasicConsume("reventuki", true, _consumer);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+        public async Task<string> do_something(string date)
+        {
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetStringAsync($"http://localhost:59165/weatherforecast/{date}");
+                return result;
+
+            }
+        }
+
+        private void DeliveredToConsole(string message)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("violento", false, false, false, null);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "violento", null, body);
+                }
+            }
+        }
+    }
+}
