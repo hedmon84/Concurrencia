1c14d0efed5f2ce0b7297526a3bf493804f893cb
Vincent Barrios <32968323+vincentsbarrios@users.noreply.github.com>
2021-03-04 17:58:32 -0800

    done

--- WeatherForecastApp-master/WeatherConsole/Program.cs
+++ WeatherForecastApp-master/WeatherConsole/Program.cs
@@ -1,22 +1,35 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
-using WeatherConsole.Background;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 namespace WeatherConsole
 {
-    class Program
+    public class Program
     {
 
-        static void Main(string[] args)
+        IServiceCollection service;
+        
+
+        public static void Main(string[] args)
         {
+            var mmessage = "";
+
+
             Console.WriteLine("Weather Console!");
             Console.WriteLine("Write elements:");
             string user_date = Console.ReadLine();
             DateDelivered(user_date);
+
+            Console.ReadLine();
         }
+
 
 
         private static void DateDelivered(string message)
@@ -30,19 +43,25 @@ namespace WeatherConsole
             {
                 using (var channel = connection.CreateModel())
                 {
-                    channel.QueueDeclare("reventuki", false, false, false, null);
+                    channel.QueueDeclare("delivered", false, false, false, null);
                     var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", "delivered", null, body);
 
-                    channel.BasicPublish("", "reventuki", null, body);
+                    channel.QueueDeclare("send", false, false, false, null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += async (model, content) =>
+                    {
+                        var body = content.Body.ToArray();
+                        var mmessage = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(mmessage);
+                    };
+
+                    channel.BasicConsume("send", true, consumer);
                 }
             }
-        }
 
-        private static void ReceiveWeather(string message)
-        {
             
         }
-
 
     }
 }
